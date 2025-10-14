ac58778387eac28cc21264dcdb6874bed151a80c
Eiler Gomez <gomezeiler250@gmail.com>
2025-10-13 21:54:12 -0600

    edicion de diseño

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    transformer: 'postcss',   // 👈 fuerza PostCSS en lugar de lightningcss
-  },
-})
+  plugins: [react(), tailwind()],
+});

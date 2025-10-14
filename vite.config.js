8ef12ac48ccb7da7e43b7ffa2a29f162db6d73e0
Eiler Gomez <gomezeiler250@gmail.com>
2025-10-13 21:46:31 -0600

    edicion de diseño

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
+  css: {
+    transformer: 'postcss',   // 👈 fuerza PostCSS en lugar de lightningcss
   },
 })
